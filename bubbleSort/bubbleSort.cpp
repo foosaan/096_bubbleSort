7b7cc3c890c33d7c6c0138bb71b06d6f3fbb9f44
foosaan <fauzan4297@gmail.com>
2023-03-17 16:55:46 +0700

    prosedur untuk menampilkan hasil

--- bubbleSort/bubbleSort.cpp
+++ bubbleSort/bubbleSort.cpp
@@ -47,3 +47,15 @@ void unsorted() {
 	}
 	cout << endl;
 }
+
+void display() {											//prosedur untuk menampilkan hasil
+	cout << endl;											//output baris kosong
+	cout << "============" << endl;							//output ke layar
+	cout << "element array yang telah tersusun" << endl;	//output ke layar
+	cout << "============" << endl;							//output ke layar
+	for (int j = 0; j < n; j++) {							//looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;								//output ke layar
+	}
+	cout << endl;											//output baris kosong
+}
+
